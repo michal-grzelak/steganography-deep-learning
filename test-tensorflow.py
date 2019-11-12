125fa611a2c9e7fc5d89f6b8191bbe0ac9f118d0
Michal Grzelak <grzelakm@outlook.com>
2019-11-12 15:28:16 +0100

    Update test script

--- test-tensorflow.py
+++ test-tensorflow.py
@@ -4,7 +4,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Activation, Dense, Conv2D, MaxPooling2D, Flatten, Reshape
+from tensorflow.python.keras.layers.core import Dense, Dropout, Flatten, Activation, Reshape
+from tensorflow.python.keras.layers.convolutional import Conv2D, MaxPooling2D, SeparableConv2D
 
 # Helper libraries
 import numpy as np
@@ -15,10 +16,14 @@ import os
 from shutil import copyfile
 import collections
 import random
+import timeit
+import statistics
+import pickle
+from sklearn.utils import shuffle
 
 import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # pgm images reader
 
@@ -52,31 +57,6 @@ def get_files_in_directory(dir, format='*', recursive=False):
     files = file_regex if not recursive else '**\\' + file_regex
     return [f for f in glob.glob(dir + files, recursive=recursive)]
 
-# arrays specifying folders for images and labels for each folder
-# trains_paths = [
-#     ["C:\Development\Coding\steganography-deep-learning\\raise\cover_pgm\\train\\", 0],
-#     ["C:\Development\Coding\steganography-deep-learning\\raise\stego_hugo_0.4\\train\\", 1]
-# ]
-
-
-# test_paths = [
-#     ["C:\Development\Coding\steganography-deep-learning\\raise\cover_pgm\\test\\", 0],
-#     ["C:\Development\Coding\steganography-deep-learning\\raise\stego_hugo_0.4\\test\\", 1]
-# ]
-trains_paths = [
-    ["C:\Development\Coding\steganography-deep-learning\Corel10k\cover_pgm\\126x187\\train\\", 0],
-    ["C:\Development\Coding\steganography-deep-learning\Corel10k\stego_uniward_0.4\\126x187\\train\\", 1],
-    # ["C:\Development\Coding\steganography-deep-learning\Corel10k\stego_wow_0.4\\126x187\\train\\", 1],
-    # ["C:\Development\Coding\steganography-deep-learning\Corel10k\stego_uniward_0.4\\126x187\\train\\", 1],
-]
-
-test_paths = [
-    ["C:\Development\Coding\steganography-deep-learning\Corel10k\cover_pgm\\126x187\\test\\", 0],
-    ["C:\Development\Coding\steganography-deep-learning\Corel10k\stego_uniward_0.4\\126x187\\test\\", 1],
-    # ["C:\Development\Coding\steganography-deep-learning\Corel10k\stego_wow_0.4\\126x187\\test\\", 1],
-    # ["C:\Development\Coding\steganography-deep-learning\Corel10k\stego_uniward_0.4\\126x187\\test\\", 1],
-]
-
 # data loader, return data in format (images, labels),
 # where images is 2d numpy array and labels is 1d numpy array
 
@@ -89,11 +69,12 @@ def load_data(paths):
         seti = []
         files = get_files_in_directory(path[0])
 
-        for file in files:
+        for i in range(len(files)):
             try:
-                seti.append(read_pgm(file))
+                image = read_pgm(files[i])
+                seti.append(image)
             except:
-                print(file)
+                print(files[i])
 
         labels = np.concatenate(
             (labels, np.full(len(seti), path[1], dtype=int)), axis=0)
@@ -102,74 +83,49 @@ def load_data(paths):
 
     return (np.array(images), labels)
 
-def set_seed(seed): 
+
+def set_seed(seed):
     tf.random.set_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
-    os.environ['PYTHONHASHSEED']=str(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
 
-
-def train():
-    # load train and test data
-    (train_images, train_labels) = load_data(trains_paths)
-    (test_images, test_labels) = load_data(test_paths)
-
-    class_names = ['cover', 'stego']
-
-    # load mnist
-    # class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-    #            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-    # (train_images, train_labels), (test_images, test_labels) = keras.datasets.fashion_mnist.load_data()
-
-
-
-    # scale values to a range 0 -> 1
-    # train_images = train_images / 255.0
-    # test_images = test_images / 255.0
-
-    # random indice shuffle for test image display
-    # indices = np.arange(train_labels.shape[0])
-    # np.random.shuffle(indices)
-
-    # plt.figure(figsize=(10,10))
-    # for i in range(25):
-    #     plt.subplot(5,5,i+1)
-    #     plt.xticks([])
-    #     plt.yticks([])
-    #     plt.grid(False)
-    #     plt.imshow(train_images[indices[i]], cmap=plt.cm.binary)
-    #     plt.xlabel(class_names[train_labels[indices[i]]])
-    # plt.show()
-
-    train_images = np.expand_dims(train_images, axis=3)
-    test_images = np.expand_dims(test_images, axis=3)
-
-    BATCH_SIZE = 64
-    SHUFFLE_BUFFER_SIZE = 100
-
-    # train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-    # test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-    # train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-    # test_dataset = test_dataset.batch(BATCH_SIZE)
-
-
-
-    input_shape = train_images.shape[1:]
-    
-    model = keras.Sequential([
-        Conv2D(filters=1, kernel_size=(3, 3),
-                            input_shape=input_shape, activation='tanh'),
-        Conv2D(filters=64, kernel_size=tuple(np.subtract(input_shape[:-1], (3, 3))), activation='tanh'),
-        Reshape((64, 2, 2)),
-        Dense(64, input_shape=(2, 2)),
+def model1(input_shape, class_names):
+    return keras.Sequential([
+        Conv2D(filters=32, kernel_size=(5, 5),
+               input_shape=input_shape, activation='tanh', kernel_initializer='glorot_uniform'),
+        MaxPooling2D((2, 2)),
+        Conv2D(filters=64, kernel_size=(3, 3), activation='relu', kernel_initializer='glorot_uniform'),
+        MaxPooling2D((2, 2)),
+        Conv2D(filters=128, kernel_size=(3, 3), activation='relu', kernel_initializer='glorot_uniform'),
+        MaxPooling2D((2, 2)),
         Flatten(),
-        Dense(len(class_names), activation='softmax')
+        Dense(128, activation='tanh', kernel_initializer='glorot_uniform'),
+        Dense(len(class_names), activation='softmax', kernel_initializer='glorot_uniform')
     ])
 
-    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.0008),
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
+def model2(input_shape, class_names):
+    return keras.Sequential([
+        Conv2D(filters=1, kernel_size=(3, 3),
+                            input_shape=input_shape, activation='tanh', kernel_initializer='glorot_uniform'),
+        Conv2D(filters=64, kernel_size=tuple(np.subtract(input_shape[:-1], (3, 3))), activation='tanh', kernel_initializer='glorot_uniform'),
+        Reshape((64, 2, 2)),
+        Dense(64, input_shape=(2, 2), kernel_initializer='glorot_uniform'),
+        Flatten(),
+        Dense(len(class_names), activation='softmax', kernel_initializer='glorot_uniform'
+        )
+    ])
+
+def train(train_images, train_labels, test_images, test_labels, class_names, learning_rate, save_name, epochs):
+    input_shape = train_images.shape[1:]
+
+    model = model2(input_shape, class_names)
+
+    model.summary()
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
     
     checkpoint_path = "training_1/cp-{epoch:04d}.ckpt"
     checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -177,30 +133,56 @@ def train():
 
     # Create a callback that saves the model's weights
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                    save_weights_only=True,
-                                                    verbose=1)
+                                                     save_weights_only=True,
+                                                     verbose=1)
 
     # model.load_weights(load_path)
-    model.load_weights("init1.h5")
-    history = model.fit(train_images, train_labels, epochs=150, callbacks=[cp_callback], validation_data=(test_images,test_labels))
+    # model.load_weights("init1.h5")
+    model.save_weights("{}.h5".format(save_name))
+    class_weight = {0: 0.5, 1: 0.5}
 
-    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+    BATCH_SIZE = 64
+    SHUFFLE_BUFFER_SIZE = 100
 
-    model.save('model-best.h5') 
+    train_images, train_labels = shuffle(train_images, train_labels)
+    
+    history = model.fit(train_images, train_labels, epochs=epochs, callbacks=[
+                        cp_callback], validation_data=(test_images, test_labels), class_weight=class_weight)
+    model.save("{}-{}.h5".format(save_name, "trained"))
+    
+    return model, history
 
+def findb():
+    (train_images, train_labels), (test_images, test_labels) = pickle.load(open("data/32x32/data_small.pkl", "rb"))
+
+    class_names = ['cover', 'stego']
+
+    # train_images = train_images / 255.0
+    # test_images = test_images / 255.0
+
+    train_images = np.expand_dims(train_images, axis=3)
+    test_images = np.expand_dims(test_images, axis=3)
+
+    # model, history = train(train_images, train_labels, test_images, test_labels, class_names, 0.001, "find/{}".format("init"), 1000)
+    model, history = train(train_images, train_labels, test_images, test_labels, class_names, 0.002, "find/{}".format("init"), 300)
+    model.save_weights("trained-1000.h5")
 
     # plot train accuracy
     plt.plot(history.history['accuracy'], label='accuracy')
-    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.ylim([0.3, 1.0])
     plt.legend(loc='lower right')
     plt.show()
 
-
-    print('\nTest accuracy:', test_acc)
+    # for i in range(100):
+    #     model, history = train(train_images, train_labels, test_images, test_labels, class_names, 0.00008, "find/{}".format(i))
+    #     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+    #     if history.history['accuracy'][-1] > 0.58:
+    #         print("Found weights that goes >0.58 for i: {}".format(i))
+    #         model.save("{}-{}.h5".format("find/found/{}".format(i), "trained"))
 
 
 with tf.device('/device:gpu:0'):
-    train()
+    findb()
